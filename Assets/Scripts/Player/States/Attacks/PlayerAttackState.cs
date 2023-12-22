8088cd21e780484f3f65fedd2a67db80079b8d32
Udayan Senapati <udayansenapati@gmail.com>
2023-12-21 19:19:31 -0500

    Started Combo Attacks

--- Assets/Scripts/Player/States/Attacks/PlayerAttackState.cs
+++ Assets/Scripts/Player/States/Attacks/PlayerAttackState.cs
@@ -8,45 +8,85 @@ namespace Player.States.Attacks
     [CreateAssetMenu(menuName = "States/Player/Attack")]
     public class PlayerAttackState : State<PlayerStateMachine>
     {
-        [SerializeField] private AttackSO[] _attackDatas;
+        [SerializeField] private AttackSO[] attackDatas;
         
         private int _currentAttackIndex;
         private AttackSO _attackData;
         private float _previousFrameTime;
 
+        private void OnValidate()
+        {
+            // this will automatically change the next combo index according to the 
+            // ordering of the attack scriptable objects on the editor
+            for (var i = 0; i < attackDatas.Length; i++)
+            {
+                // if it's the last one, the next one is the first one so we loop the animations
+                if (i == attackDatas.Length - 1)
+                {
+                    attackDatas[i].nextComboIndex = 0;
+                }
+                else
+                {
+                    attackDatas[i].nextComboIndex = i + 1;
+                }
+            }
+        }
+
         public override void Enter(PlayerStateMachine parent)
         {
             base.Enter(parent);
-            // Play Animations Here
+
+            // we need to gather which is the current attack we want
+            _attackData = attackDatas[_currentAttackIndex];
+
             parent.Animations.PlayAttack(_attackData.attackName);
         }
 
         public override void Tick(float deltaTime)
         {
+            var normalizedTime = runner.Animations.GetNormalizedTime();
+
+            if (normalizedTime >= _previousFrameTime && normalizedTime < 1f)
+            {
+                if (runner.MeleeAttackPressed)
+                {
+                    TryComboAttack(normalizedTime);
+                }
+            }
+            else
+            {
+                // go back to idle
+                runner.SetState(typeof(PlayerIdleState));
+            }
+
+            _previousFrameTime = normalizedTime;
         }
 
         public override void AnimationTriggerEvent(AnimationTriggerType triggerType)
         {
             base.AnimationTriggerEvent(triggerType);
 
-            if (triggerType == AnimationTriggerType.FinishAttack)
-            {
-                if (runner.MeleeAttackPressed)
-                {
-                    // Next Combo
-                }
-                else
-                {
-                    runner.SetState(typeof(PlayerIdleState));
-                }
-            }
-
             if (triggerType != AnimationTriggerType.HitBox) return;
-            
-            // Check for Collsions
+
             var colliders = _attackData.Hit(runner.transform, runner.IsFacingRight);
 
             PerformDamage(colliders);
+        }
+
+        private void TryComboAttack(float normalizedTime)
+        {
+            if (normalizedTime < _attackData.comboAttackTime)
+            {
+                return;
+            }
+
+            // we clone the current state using the extension we created
+            var newCombo = this.Clone();
+
+            // we set the current index to the next one in the list
+            newCombo._currentAttackIndex = _attackData.nextComboIndex;
+
+            runner.SetState(newCombo);
         }
 
         private void PerformDamage(Collider[] colliders)
