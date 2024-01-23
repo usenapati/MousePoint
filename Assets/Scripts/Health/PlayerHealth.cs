5b88ecaf0ea4e09a2e70e69c7cb42068f8d74731
Udayan Senapati <udayansenapati@gmail.com>
2024-01-23 18:39:25 -0500

    Started Respawn Manager

---
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Health
+{
+    public class PlayerHealth : Health
+    {
+        
+        protected override void Die()
+        {
+            // Respawn
+        }
+    }
+}
+
