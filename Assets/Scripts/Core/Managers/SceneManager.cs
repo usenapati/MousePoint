8b43bcf998d91e5c163cde6e65d9c289c1512e97
gabbie mouse <136210796+glitchmouse@users.noreply.github.com>
2024-01-31 18:59:39 -0500

    Main menu and cut scene flow
    - main menu
    - scene transition manager script
    - boat scene
    - boat scene script
    
    clicking start in the main menu triggers a fade to the boat scene which plays, then fades to totten town

--- Assets/Scripts/Core/Managers/SceneManager.cs
+++ Assets/Scripts/Core/Managers/SceneManager.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    internal static void LoadScene(string gameplaySceneName)
+    {
+        throw new NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
